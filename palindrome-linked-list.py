c26b8a1bf4f1e9ee7ea13bd111795a0d2438370a
zeus0007 <wpdn79@likelion.org>
2022-03-11 16:41:40 +0900

    palindrome-linked-list

--- palindrome-linked-list.py
+++ palindrome-linked-list.py
@@ -1,16 +1,17 @@
 # linked list가 팰린드롬 구인 판별
 # 팰린드롬 구조 앞으로해도 뒤로 해도 똑같은 자료구조
 from qa_system import QA
+
 def answer_function(input_string):
     input_list = input_string.split('->')
     input_string = ''.join(input_list)
     back_string = input_string[::-1]
-    print(input_string, back_string)
+
     if input_string == back_string:
         return 'true'
     else :
         return 'false'
-        
+
 qa1 = QA('1->2', 'false')
 qa1.check_answer(answer_function)
 qa2 = QA('1->2->2->1', 'true')
