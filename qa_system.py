a55255f229fc4ba16798914b146411ed91db53ce
zeus0007 <wpdn79@likelion.org>
2022-03-11 16:41:14 +0900

    feat: 실행시간 ms 단위로 변경

--- qa_system.py
+++ qa_system.py
@@ -8,8 +8,9 @@ class QA():
         start_time = time.time()
         result = answer_function(self.question)
         end_time = time.time()
+        run_time = (end_time - start_time)*1000
         if self.answer == result:
-            print(f'''입력 : {self.question}\n답안 : {result}\n실행시간 : {end_time - start_time:.5f} sec\n맞았습니다.\n''')
+            print(f'''입력 : {self.question}\n답안 : {result}\n실행시간 : {run_time:.5f} ms\n맞았습니다.\n''')
         else:
-            print(f'''입력 : {self.question}\n답안 : {result}\n실행시간 : {end_time - start_time:.5f} sec\n틀렸습니다.\n''')
+            print(f'''입력 : {self.question}\n답안 : {result}\n실행시간 : {run_time:.5f} ms\n틀렸습니다.\n''')
         
