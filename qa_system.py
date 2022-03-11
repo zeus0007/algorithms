d0bed311719f731940302bca7c7b6ac37a728cd0
zeus0007 <wpdn79@likelion.org>
2022-03-11 16:39:52 +0900

    feat: 실행시간 측정 포함

--- qa_system.py
+++ qa_system.py
@@ -1,11 +1,15 @@
+import time
 class QA():
     def __init__(self, question, answer):
         self.question = question
         self.answer = answer
 
     def check_answer(self, answer_function):
+        start_time = time.time()
         result = answer_function(self.question)
+        end_time = time.time()
         if self.answer == result:
-            print(self.question, '맞았습니다.')
+            print(f'''입력 : {self.question}\n답안 : {result}\n실행시간 : {end_time - start_time:.5f} sec\n맞았습니다.\n''')
         else:
-            print(self.question, '틀렸습니다.')
+            print(f'''입력 : {self.question}\n답안 : {result}\n실행시간 : {end_time - start_time:.5f} sec\n틀렸습니다.\n''')
+        
